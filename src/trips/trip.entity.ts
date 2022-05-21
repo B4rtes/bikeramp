992c054e1fc7e52a8677d7fe25238733dacf006f
Your Name <you@example.com>
2022-05-21 22:29:45 +0200

    Add trip db entity

---
+++ src/trips/trip.entity.ts
@@ -0,0 +1,34 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+class Trip {
+  @PrimaryGeneratedColumn()
+  public id: number;
+
+  @Column({
+    type: "varchar"
+  })
+  public start_address: string;
+
+  @Column({
+    type: "varchar"
+  })
+  public end_address: string;
+
+  @Column({
+    type: "float"
+  })
+  public distance: number;
+
+  @Column({
+    type: "decimal"
+  })
+  price: number;
+
+  @Column({
+    type: "date"
+  })
+  date: Date;
+}
+
+export default Trip;
