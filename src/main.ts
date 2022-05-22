58c2ef132c2c628e272073aab20c1cc542523f37
Your Name <you@example.com>
2022-05-22 13:50:28 +0200

    Add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const GLOBAL_PREFIX = 'api';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix(GLOBAL_PREFIX);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    // disableErrorMessages: true,
+  }));
   await app.listen(3000);
 }
 bootstrap();
